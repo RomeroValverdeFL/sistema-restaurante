2d50b580c9d688b39002a7099de6911aaa592487
lromerov <romero__valverde__rvnn@hotmail.com>
2019-11-25 14:53:03 -0500

    Lunes 25 de nov 2

--- src/main/java/pe/dashboard/SistemaIntegralRestaurante/controller/ContactoVentasController.java
+++ src/main/java/pe/dashboard/SistemaIntegralRestaurante/controller/ContactoVentasController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import pe.dashboard.SistemaIntegralRestaurante.model.entity.Contactame;
-
+import pe.dashboard.SistemaIntegralRestaurante.model.entity.Menu;
 import pe.dashboard.SistemaIntegralRestaurante.service.ContactoService;
 import pe.dashboard.SistemaIntegralRestaurante.service.EstadoService;
 
@@ -33,6 +36,18 @@ public class ContactoVentasController {
 			// TODO: handle exception
 		}
 		return "/contact";
+	}
+	@PostMapping("/save")
+	public String save(@ModelAttribute("contacto") Contactame contacto, 
+			Model model, SessionStatus status) {
+		try {
+			contactoService.save(contacto);
+			status.setComplete();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/contact";
 	}
 
 }
